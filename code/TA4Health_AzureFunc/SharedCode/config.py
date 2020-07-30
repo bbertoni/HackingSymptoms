51f37e55ea36c1893120123de6a0eb87750948ba
bbertoni <bridget.bertoni@gmail.com>
2020-07-30 13:48:10 -0500

    added zipped Azure function files

--- code/TA4Health_AzureFunc/SharedCode/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# for local use only (uncomment below)
-#text_analytics_endpoint = "<YOUR TEXT ANALYTICS FOR HEALTH APP SERVICE URL HERE>/text/analytics/v3.0-preview.1/domains/health"
-
-# for deployed use (uncomment below)
-text_analytics_endpoint = str(os.environ["text_analytics_endpoint"])
